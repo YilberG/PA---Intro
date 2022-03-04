7565c945d5e51e0824fa429ba26158dddc9e9573
YilberG <yilber-99@hotmail.com>
2022-03-03 21:52:55 -0500

    nueva base de datos

--- db.sql
+++ db.sql
@@ -12,7 +12,27 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura de base de datos para baseproductos
+CREATE DATABASE IF NOT EXISTS `baseproductos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `baseproductos`;
+
+-- Volcando estructura para tabla baseproductos.productos
+CREATE TABLE IF NOT EXISTS `productos` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(255) NOT NULL,
+  `Precio` decimal(15,2) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla baseproductos.productos: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` (`Id`, `Nombre`, `Precio`) VALUES
+	(1, 'yilber', 2000.00),
+	(2, 'camilo', 3500.00),
+	(3, 'guevara', 56000.00),
+	(4, 'gh', 324.00);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
